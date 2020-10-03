acd2d385dbb93e6b6a8b4ede53e5eda3296f1642
Tim de Jager <tdejager89@gmail.com>
2020-10-03 13:23:36 +0200

    feat: first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use bracket_lib::prelude::*;
+
+struct State { }
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Sjoerd!");
+    }
+}
+
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("SjoeRPG - Play your life!")
+        .build()?;
+
+    main_loop(context, State{})
+}
